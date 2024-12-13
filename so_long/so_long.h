62f4d37ad68b3a497cd15b28081b5d0c208f871f
dsteiger42 <167896122+dsteiger42@users.noreply.github.com>
2024-12-13 21:43:40 +0000

    Update so_long.h

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,20 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dsteiger <dsteiger@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/30 16:25:31 by dsteiger          #+#    #+#             */
-/*   Updated: 2024/11/30 16:27:57 by dsteiger         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+# define WALLS '1'
+# define FLO0R '0'
+# define COLLECTIBLE 'C'
+# define EXIT 'E'
+# define START_POS 'P'
 
 # define W 13
 # define A 0
 # define S 1
 # define D 2
+
 # define UARROW 126
 # define LARROW 123
 # define DARROW 125
 # define RARROW 124
+
+# include "libft/libft.h"
+# include "printf/ft_printf.h"
+# include "so_long.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_map
+{
+    char **map;
+    int fd;
+    int height;
+    int width;
+    int player_count;
+    int collectibles_count;
+    int exit_count;
+    void *mlx;
+    void *win;
+
+}   t_map;
+
+#endif
