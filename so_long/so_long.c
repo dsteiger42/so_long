dde7155b0b54083c83a5c2b3e6194d92aecd019c
dsteiger42 <167896122+dsteiger42@users.noreply.github.com>
2024-12-22 23:36:15 +0000

    Update so_long.c

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -1,25 +1,45 @@
 #include "so_long.h"
 
-static int	argv_checker(char *argv)
+static void	*ftt_memset(void *b, int c, size_t length)
 {
-	if (!argv)
-		return (0);
-	check_errors(argv);
-    check_map_name(argv);
-	return (0);
+	unsigned char	*p;
+
+	p = (unsigned char *)b;
+	while (length--)
+		*p++ = (unsigned char)c;
+	return (b);
 }
 
 int main(int argc, char **argv)
 {
-    if (argc == 2)
-    {
-//      check_errors(argv);
-//      check_map_name(argv);
-    }
-    else
-    {
-        ft_printf("Wrong number of arguments!");
-        return (0);
-    }
+    t_vars vars;
 
+    if (argc != 2)
+        return (1);
+    if (check_map_name(argv[1]))
+        return (1);
+    ftt_memset(&vars, 0, sizeof(t_vars));
+    vars.map_file = argv[1];
+    if (!map_reading(&vars))
+        return (1);
+    check_errors(&vars);
+    /*if (!map_checker(&vars))
+	{
+		printf("Error: Map contains invalid characters.\n");
+		return (1);
+	}*/
+    printf("Map loaded successfully.\n");
+    vars.mlx = mlx_init();
+    if (!vars.mlx)
+        return (1);
+    vars.win = mlx_new_window(vars.mlx, 1920, 800, "Game so_long");
+    if (!vars.win)
+        return (1);
+    load_images(&vars);
+    draw_map(&vars);
+    mlx_hook(vars.win, 17, 0, (void *)exit_game, &vars);
+    mlx_key_hook(vars.win, handle_keypress, &vars);
+    mlx_loop(vars.mlx);
+    return (0);
 }
+
