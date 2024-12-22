c48669e9da316fb94cbee07799ff60f013db15fc
dsteiger42 <167896122+dsteiger42@users.noreply.github.com>
2024-12-22 23:35:46 +0000

    Update file.c

--- so_long/file.c
+++ so_long/file.c
@@ -1,20 +1,20 @@
 #include "so_long.h"
 
-void check_map_name(char *map_name)
+int check_map_name(char *map_name)
 {
-    t_map   file;
-	int		len;
+    int len;
 
-	if (!map_name)
-		ft_printf("ERROR: Invalid pointer to file name\n");
-	file.fd = open(map_name, O_RDONLY);
-	if (file.fd < 0)
-		ft_printf("ERROR: Error opening the file\n");
-	len = ft_strlen(map_name);
-	if (len < 4 || ft_strncmp(&map_name[len - 4], ".ber", 4) != 0) // If != 0, it means the strings are different.
-		ft_printf("ERROR: Invalid map name, should end with .ber\n");
-	else
-		ft_putstr_fd("Check name passed\n", 1);
-	close(file.fd);
-
+    if (!map_name)
+    {
+        printf("ERROR: Invalid pointer to file name\n");
+        return (1);
+    }
+    len = strlen(map_name);
+    if (len < 4 || strncmp(&map_name[len - 4], ".ber", 4) != 0)
+    {
+        printf("ERROR: Invalid map name, should end with .ber\n");
+        return (1);
+    }
+    return (0);
 }
+
